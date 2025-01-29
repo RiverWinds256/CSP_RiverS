9988fadb94d3b6022071948ee808e82d3016a08a
RiverWinds256 <river.stanley@ucas-edu.net>
2025-01-29 11:43:03 -0700

    I did pyton practice

--- Variables/Notes.py
+++ Variables/Notes.py
@@ -1,3 +1,15 @@
 # River Stanley, Variable 
 
-print("it worked")
+name = "River"
+
+subject = input ("do you have a dog: ")
+
+small_number = 4
+
+name = input("whats their name: ")
+
+print(name, "has a dog named", subject)
+
+
+name = "alex"
+
