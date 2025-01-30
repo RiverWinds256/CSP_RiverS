9b10feab743f08e34d9f1664b8b4e53d2a521ec4
RiverWinds256 <river.stanley@ucas-edu.net>
2025-01-30 11:49:58 -0700

    C code

---
+++ Variables/practice.py/notes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+char name [] = "river";
+int age = 15;
+ float pi = 3.14;
+
+int main(void){
+    printf("Hello I am%s/n", name);
+    printf("%d/n", age);
+   printf("%f\n", pi);
+   
+return 0;
+}
