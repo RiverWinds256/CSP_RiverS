d3031c6b179a0e121a3b668e7d96b088d547f3cf
RiverWinds256 <river.stanley@ucas-edu.net>
2025-02-05 22:08:47 +0000

    Finished pyton code +varaiables

--- Expressions/expressionpractice.py
+++ Expressions/expressionpractice.py
@@ -1,11 +1,12 @@
 #River Stanley expression practice 
 
+three=3
 print(7-24/8*4+6)
-print(18/3-7+2*5)
+print(18/three-7+2*5)
 print(6*4/12+72/8-9)
-print((17-6/2)+4*3)
-print(-2*(1*4-2/2)+(6+2-3))
-print(-1*((3-4*7)/5)-2*24/6)
-print((3*5**2/15)-(5-2**2))
-print((1**4*2**2+3**3)-2**5/4)
+print((17-6/2)+4*three)
+print(-2*(1*4-2/2)+(6+2-three))
+print(-1*((three-4*7)/5)-2*24/6)
+print((three*5**2/15)-(5-2**2))
+print((1**4*2**2+three**three)-2**5/4)
 print((22/2-2*5)**2+(4-6/6)**2)
