fc3867bbacad2b107661a00e062223d6aaaf735e
RiverWinds256 <river.stanley@ucas-edu.net>
2025-02-05 22:38:57 +0000

    Part way done with expression practice

--- Expressions/expressionpractice.c
+++ Expressions/expressionpractice.c
@@ -1,12 +1,28 @@
 // River Stanley Expression practice
+//nick says i must start from scratch :(
 
 
 #include <stdio.h>
 #include <math.h>
+float power(float base,int power)
+{
+    float rv=1.0;
+    int i=1;
+    for (; i<= power; i++) {
+        rv=rv*base;
+    }
+}
+
+int main(void){ 
+    printf("%d\n",7-24/8*4+6);
+    printf("%d\n",18/3-7+2*5);
+    printf("%d\n",6*4/12+72/8-9);
+    printf("%d\n",(17-6/2)+4*3);
+    printf("%d\n",-2*(1*4-2/2)+(6+2-3));
+    printf("%d\n",-1*((3-4*7)/5)-2*24/6);
+    printf("%d\n")
 
 
-int main(void){
-    printf("Hello World");
 return 0;
 }
 
