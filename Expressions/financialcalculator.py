320a8140b335be96ff3472b21951114936360af6
RiverWinds256 <river.stanley@ucas-edu.net>
2025-02-06 01:33:57 +0000

    Started on financial calculator

---
+++ Expressions/financialcalculator.py
@@ -0,0 +1,8 @@
+#River Stanley financial calculator 
+
+print("welcome to the finance calculator")
+income=float(input("what is your income? "))
+saving=income*0.1
+rent=float(input("how much do you spend on rent? "))
+utilities=float(input(""))
+
