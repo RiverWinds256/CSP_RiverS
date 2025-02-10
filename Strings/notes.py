88e02a409ac06dfed19daac8596a21ed79ce157b
RiverWinds256 <river.stanley@ucas-edu.net>
2025-02-10 11:47:42 -0700

    Notes for strings

---
+++ Strings/notes.py
@@ -0,0 +1,17 @@
+# River Stanley, String Notes Python 
+
+#string is a data type that holds information, surroundedby "" marks
+
+note="river's class"
+
+name=int(input(" What is your first name\n ").strip().capitalize())
+
+#print(f"hello {name} welcome to my program")
+print(f"this is your name {name}")
+
+sentence = "the quick brown fox jumps over the lazy dog"
+
+print(note)
+
+
+
