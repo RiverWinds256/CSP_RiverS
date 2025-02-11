fa1e0cb827e83d9bec3e3b70d3e107e49da64b45
RiverWinds256 <river.stanley@ucas-edu.net>
2025-02-11 11:15:40 -0700

    Notes py

--- Strings/notes.py
+++ Strings/notes.py
@@ -2,16 +2,22 @@
 
 #string is a data type that holds information, surroundedby "" marks
 
-note="river's class"
+#note="river's class"
 
-name=int(input(" What is your first name\n ").strip().capitalize())
+#name=int(input(" What is your first name\n ").strip().capitalize())
 
 #print(f"hello {name} welcome to my program")
-print(f"this is your name {name}")
+#print("this is your name"+name)
 
 sentence = "the quick brown fox jumps over the lazy dog"
 
-print(note)
+#print(note)
+
+#print(len(sentence))
+
+#print(sentence [4])
 
 
-
+start = (sentence.find("brown"))
+length = len("brown fox")
+print(sentence[10:19])
