160c87ea4e325c0751741abc988062178877cf09
RiverWinds256 <river.stanley@ucas-edu.net>
2025-02-12 11:36:30 -0700

    Finshed name decor

--- Strings/namedecorator.c
+++ Strings/namedecorator.c
@@ -2,9 +2,23 @@
 
 
 #include <stdio.h>
+#include <strings.h>
 
 int main(void){
-    printf("Hello World");
+    printf("welcome to name decorator\n");
+     char name[50]; 
+     char decor1[] = "<<<<<<";
+     char decor2 []= ">>>>>>";
+     printf("Please enter your name :");
+    scanf("%s", name);
+    strcat(decor1,name);
+    strcat(decor1, decor2);
+
+    printf("%s",decor1);
+
+    
+
+
 return 0;
 }
 
--- Strings/namedecorator.py
+++ Strings/namedecorator.py
@@ -1,1 +1,12 @@
 # River Stanley Name decorator Python
+
+print("welcome to name decorator")
+name=(input(" What is your first name\n ").strip().capitalize())
+
+string1= name
+decor= (" :0 :) :3 ")
+string2= decor
+
+decortated_name= string1 + string2
+
+print(decor+decortated_name)
