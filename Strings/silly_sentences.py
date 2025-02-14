ddc2974daa61c2ea0d7f254d3b58c532638b1141
RiverWinds256 <river.stanley@ucas-edu.net>
2025-02-14 11:21:23 -0700

    finished silly sentences C

--- Strings/silly_sentences.py
+++ Strings/silly_sentences.py
@@ -1,3 +1,13 @@
 # River Stanley, silly sentences
 
-print("welcome to the silly sentences")
+print("Welcome to the silly sentences!")
+print("Please keep responses to one word")
+
+word1 = (input("What is your favorite food? "))
+word2 = (input("What is your favorite resaurant? "))
+word3 = (input("What is your dream job? "))
+
+print("The living breathing", word1, "had came alive and escaped", word2, "in hopes of becoming a", word3, "one day.")
+
+print("Wish them luck")
+input("")
