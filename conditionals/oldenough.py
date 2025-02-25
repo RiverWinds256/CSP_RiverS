194803b83e30d8d6272773f721e57f01eb201520
RiverWinds256 <river.stanley@ucas-edu.net>
2025-02-25 22:37:07 +0000

    Old enough pyton work

---
+++ conditionals/oldenough.py
@@ -0,0 +1,22 @@
+# River Stanley Old Enough python
+
+print("welcome to the are you old enough cal")
+
+age=float(input("How old are you? "))
+
+if age >=18:    
+    print("You are old enough to go to school, get a learner permit, drive and vote.")
+
+elif age >=16: 
+     print ("You are old enough to go to school, get a learner permit, drive, but not vote")    
+
+elif age >=15:
+     print ("You are old enough to go to school, get a learner permit, but not drive and vote.")
+
+elif age >5:
+    print ("you are old enough to go to school, but not old enough get a learners permit, drive, or vote.")  
+
+elif age>=15:
+     print("print")    
+else:
+     print("you are not old enough go to school, get a learners permit, drive, or vote")     
