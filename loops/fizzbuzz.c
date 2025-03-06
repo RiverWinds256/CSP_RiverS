5659836fa8086fe69b45b8df90b1cba6f586c587
RiverWinds256 <river.stanley@ucas-edu.net>
2025-03-06 11:46:26 -0700

    Working on fizzbuzz

---
+++ loops/fizzbuzz.c
@@ -0,0 +1,25 @@
+// River Stanley TEMPLATE
+
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+   
+int x; {
+ for(x=0;x<51;x++)
+ }  
+ printf("%d\n", x);
+for x[][] in range(0,51):
+int i;
+    if x%5 ==0 and x%3 ==0:
+        print("fizzbuzz");
+    else if  x%5 ==0:
+        printf("buzz");
+    else if x%3 ==0:
+        printf("fizz");
+    else: 
+        printf(x)
+
+return 0;
+}
