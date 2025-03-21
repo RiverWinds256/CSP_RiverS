1f53f00e07df72067288b31d2f4cfe1a7354d9e6
RiverWinds256 <river.stanley@ucas-edu.net>
2025-03-21 11:07:53 -0600

    lets fix this

--- cake.py
+++ cake.py
@@ -7,13 +7,13 @@
 #Chiara What kind of cake? EX: vallinia, 
 #chocolate, strawberry
 
-#Chiara ingredients and cake mix EX: eggs, water, cakemix (This will probably be void and just for playtime)
+#Chiara what kind of frosting EX: Buttercream, cream cheese, swiss meringue
 
 #River Cake pans EX: circle, square, cupcake
 
 #River temperature EX: 325-375 normal bake, under is underbaked, over by like 20 is overbaked any further is broken and on fire oven
 
-#Lizzie frosting EX: vallinia, chocolate, strawberry 
+#Lizzie frosting EX: pipped on, spooned on, none 
 
 #Lizzie toppings EX: sprinkles, strawberries, cream
 
@@ -22,15 +22,17 @@
 
 shape=input("What kind of kind of cake will you bake? A circle cake, a square cake or cupcakes?\n").strip()
 if shape!="circle" and shape!="square" and shape!="cupcakes":
-     print("We dont have that kind of cakepan")     
+     print("We dont have that kind of cakepan\n")     
 
-temp=input("What temperature will you set the oven?")   
-if temp>=320 and temp<=375: 
-    print("Your cake is perfectly baked")
+temp=input("What temperature will you set the oven?\n")   
+if temp<320:
+     print("Your cake hasn't baked")
 elif temp<319:
-     print("Your cake is a bit unbaked")
+     print("Your cake is a little undercooked but it will work\n")
+elif temp>=320 and temp<=375: 
+    print("Your cake is perfectly baked")
 elif temp>=400 and temp<=420:
-     print("Your cake is burnt")   
+     print("Your cake is a little burnt but it will work\n")   
 else:
      print("The oven has lit on fire and burned down the bakery, game over")
      exit()
