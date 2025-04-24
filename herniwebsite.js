88b40d3529a68a70a77fc7b38b95cc921158cf75
RiverWinds256 <river.stanley@ucas-edu.net>
2025-04-24 03:50:14 +0000

    set up

--- herniwebsite.js
+++ herniwebsite.js
@@ -10,7 +10,15 @@ function leave(image){
 }
 
 function toggle(){ 
+
     const thing=document.getElementById("hidden")
+    if(thing.style.display=="none"){
+        thing.style.display=""
+
+    }else{
+        thing.style.display="none"
+    }
+
 
 
 
---
+++ javascript/hiddenimage.js
@@ -0,0 +1,11 @@
+function appear(){
+    
+    const thing=document.getElementById("hidden")
+    if(thing.style.display=="none"){
+        thing.style.display=""
+
+    }else{
+        thing.style.display="none"
+    }
+
+}
