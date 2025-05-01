6218081c03aa3e019434950dd1ae8727fee06d0a
RiverWinds256 <river.stanley@ucas-edu.net>
2025-05-01 12:57:32 -0600

    IMAGE WORKS

--- final.js
+++ final.js
@@ -1,13 +1,14 @@
 
 function point(image){
 
-    image.src="https://www.nps.gov/cany/planyourvisit/images/RhodesSmartt1_1.jpg?maxwidth=1300&autorotate=false&quality=78&format=webp"
+    image.src=" https://i0.wp.com/www.littlemissturtle.com/wp-content/uploads/2022/12/Canyonlands-National-Park-in-a-wheelchair-Little-Miss-Turtle-Wheelchair-Travel-Blog.jpg?fit=1600%2C1068&ssl=1"
 
 }
 
 function leave(image){
 
-    image.src="https://www.nps.gov/cany/planyourvisit/images/RhodesSmartt1_1.jpg?maxwidth=1300&autorotate=false&quality=78&format=webp"
+    image.src="https://images.squarespace-cdn.com/content/v1/6386355cefa1aa7a84f4a956/1670079894775-AF9WZVDDNMXP6CF9AS5A/image-asset.jpeg?format=2500w"
+
 }
 
 
